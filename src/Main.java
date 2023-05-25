0fa763ab1e775e2b0d2067869ffa1147e4174507
Godsway Hero <60008545+KojoHero@users.noreply.github.com>
2023-05-25 15:34:42 +0000

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,49 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        System.out.print("Hello and welcome!");
+        System.out.println(" this is a text");
         for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
+        }
+
+        int one = 1;
+        String name = "ama";
+        double dec = 9.0223;
+
+        System.out.println(name+dec);
+
+        char val = 65;
+        System.out.println(val);
+        int x=50, y=25, z =34;
+        System.out.println(x%z);
+
+        System.out.println(Math.abs(-4.7));
+
+        int day = 8;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enjoy your weekend");
         }
     }
 }
